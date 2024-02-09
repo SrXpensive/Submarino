9c79e2c2bbd50f63c5a64da30742e61bb18a2be2
Edu Caro <eduardo.caro.lorente@gmail.com>
2024-02-09 10:56:52 +0100

    Primer commit

---
+++ Leer.java
@@ -0,0 +1,67 @@
+package leer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+public class Leer {
+    private final static BufferedReader entradaConsola =
+            new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+
+    /*
+    *Lee un texto del teclado.
+    * @param mensaje Texto que se pasa al usuario
+    * @return el texto introducido. Blanco en caso de error
+     */
+    public static String leerTexto(String mensaje){
+        String respuesta = null;
+        do{
+            try{
+                System.out.print(mensaje);
+                respuesta = entradaConsola.readLine();
+            }
+            catch (IOException ex){
+                return "";
+            }
+        } while(respuesta == null);
+        return respuesta;
+    }
+
+    /*
+    * Introducción de números enteros
+    * @param mensaje Mensaje que se da al usuario
+    * @return un entero con el valor
+     */
+    public static int leerEntero(String mensaje){
+        int n= 0;
+        boolean conseguido = false;
+        while(!conseguido){
+            try{
+                n= Integer.parseInt(leerTexto(mensaje));
+                conseguido=true;
+            }
+            catch(NumberFormatException ex){
+                System.out.println("Debes introducir un número correcto");
+            }
+        }
+        return n;
+    }
+    /*
+    * Introducción de números decimales
+    * @param mensaje Mensaje que se da al usuario
+    * @return un decimal con el valor
+     */
+    public static double leerDouble(String mensaje){
+        double n=0.0;
+        boolean conseguido = false;
+        while (!conseguido){
+            try{
+                n=Double.parseDouble(leerTexto(mensaje));
+                conseguido = true;
+            }
+            catch(NumberFormatException ex){
+                System.out.println("Debes introducir un número correcto");
+            }
+        }
+        return n;
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String [][] tablero;
+        int opcion = 0;
+        System.out.println("LA CAZA DEL SUBMARINO\n");
+        System.out.println();
+        do{
+            System.out.println("Menú principal");
+            System.out.println("--------------");
+            System.out.println("1. Jugar");
+            System.out.println("2. Ayuda");
+            System.out.println("0. Salir");
+            System.out.println("--------------");
+            opcion = leer.Leer.leerEntero("Introduce una opción: ");
+        }while(opcion !=0);
+    }
+}
